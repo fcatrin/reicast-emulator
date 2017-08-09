2d19cb595d9b28ae76ddba55d6ca72945d91a006
Franco Catrin <fcatrin@gmail.com>
2017-08-09 12:38:14 -0400

    Back to SDK21. Cannot build without Apache HTTP, required by Dropbox

--- shell/android/src/com/reicast/emulator/config/InputModFragment.java
+++ shell/android/src/com/reicast/emulator/config/InputModFragment.java
@@ -36,7 +36,7 @@ import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-import com.reicast.emulator.R;
+import retrox.reicast.emulator.R;
 import com.reicast.emulator.periph.Gamepad;
 
 public class InputModFragment extends Fragment {
